4e6be9e67111cc747eb694d8870a225830f97545
Roxrit3 <jak.kozel@gmail.com>
2023-05-03 20:18:42 +0200

    přidal jsem komentáře

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 def main():
-  jmeno = input("zadej sve jmeno: ")#opraven input
+  jmeno = input("zadej sve jmeno: ") #opraven input
   print("Ahoj", jmeno)
 
 if __name__ == "__main__":
   main()
+#git add .
+#git commit -m "upravil jsem main"
+#git push
+#git branch
+#git branch nova_featurka - vytvoří nový branch
+#git switch nova_featurka - přepne mě do nové větve
+#ahojky pusino
